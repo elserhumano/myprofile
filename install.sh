a4523c42266febc969e01896ebc7094be790c05e
Fernando Alvarez <fernando.alvareznoya@bryxx.eu>
2020-06-12 08:47:50 +0200

    Installation file

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Backup previous files / dirs
+mv .bashrc .bashrc-backup
+mv .vimrc .vimrc-backup
+mv .vim .vim-backup
+
+# Bring the new files to live!
+mv bashrc-file-oculto .bashrc
+mv vimrc-file-oculto .vimrc
+mv vim-dir-oculto .vim
+
+echo "Installation done! Enjoy :)"
