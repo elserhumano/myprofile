1b6a4eb186ff3ff6aa1241c4bcfc6322ff467b01
Fernando <falvarez.uy@gmail.com>
2020-06-13 22:07:04 +0200

    Correct path

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Backup previous files / dirs
-mv .bashrc .bashrc-backup
-mv .vimrc .vimrc-backup
-mv .vim .vim-backup
+mv ../.bashrc ../.bashrc-backup
+mv ../.vimrc ../.vimrc-backup
+mv ../.vim ../.vim-backup
 
 # Bring the new files to live!
-mv bashrc-file-oculto .bashrc
-mv vimrc-file-oculto .vimrc
-mv vim-dir-oculto .vim
+mv bashrc-file-oculto ../.bashrc
+mv vimrc-file-oculto ../.vimrc
+mv vim-dir-oculto ../.vim
 
 echo "Installation done! Enjoy :)"
+
